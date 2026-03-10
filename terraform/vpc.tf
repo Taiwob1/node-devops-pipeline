caa815f6734036f86243ef6aaf673241079d8737
Taiwo Babalola <taiwobabalola452@yahoo.com>
2026-03-10 08:33:48 -0500

    fix: add internet gateway and route table to VPC

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -15,3 +15,30 @@ resource "aws_subnet" "public2" {
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 }
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Associate Route Table with Subnet 1
+resource "aws_route_table_association" "public_rta" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Associate Route Table with Subnet 2
+resource "aws_route_table_association" "public_rta2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_rt.id
+}
