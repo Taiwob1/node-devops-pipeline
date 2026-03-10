67a12247c8a37d5854c7671d1c5b82706aa7efe1
Taiwo Babalola <taiwobabalola452@yahoo.com>
2026-03-10 09:01:59 -0500

    fix: add cloudwatch logs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,9 @@
+# CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/node-app"
+  retention_in_days = 7
+}
+
 # ECS Cluster
 resource "aws_ecs_cluster" "main" {
   name = "node-devops-cluster"
@@ -10,6 +16,7 @@ resource "aws_ecs_task_definition" "node_app" {
   memory                   = "512"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -20,8 +27,37 @@ resource "aws_ecs_task_definition" "node_app" {
         containerPort = 3000
         hostPort      = 3000
       }]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/node-app"
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
     }
   ])
+}
+
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # ECS Service
